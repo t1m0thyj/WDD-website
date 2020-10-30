58b57a9ae84f1f032f623541ed272b43ab1e4310
Timothy Johnson <tandrewjohnson@outlook.com>
2020-10-29 22:43:08 -0400

    Redirect scripts page to WDD-scripts repo

--- build.py
+++ build.py
@@ -3,21 +3,21 @@ import os
 
 from mako.template import Template
 
-for filename in os.listdir("scripts"):
-    if os.path.isfile(f"scripts/{filename}"):
-        os.remove(os.path.join("scripts", filename))
+# for filename in os.listdir("scripts"):
+#     if os.path.isfile(f"scripts/{filename}"):
+#         os.remove(os.path.join("scripts", filename))
 
-with open("script-data.json", 'r') as fileobj:
-    script_data = json.load(fileobj)
+# with open("script-data.json", 'r') as fileobj:
+#     script_data = json.load(fileobj)
 
-with open("scripts.mako", 'r') as fileobj:
-    scripts_template = Template(fileobj.read())
+# with open("scripts.mako", 'r') as fileobj:
+#     scripts_template = Template(fileobj.read())
 
-with open("scripts/index.html", 'w') as fileobj:
-    fileobj.write(scripts_template.render(scripts_stable=script_data["stable"],
-        scripts_experimental=script_data["experimental"]))
+# with open("scripts/index.html", 'w') as fileobj:
+#     fileobj.write(scripts_template.render(scripts_stable=script_data["stable"],
+#         scripts_experimental=script_data["experimental"]))
 
-print("Generated scripts page:", "./scripts/index.html")
+# print("Generated scripts page:", "./scripts/index.html")
 
 for filename in os.listdir("themes"):
     if os.path.isfile(f"themes/{filename}"):
