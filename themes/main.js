6825803da7aa47b96d59591f78cba2a09f584bae
Timothy Johnson <tandrewjohnson@outlook.com>
2024-03-29 13:21:36 -0400

    Add new themes

--- themes/main.js
+++ themes/main.js
@@ -72,7 +72,7 @@ function loadThumbnailGrid(themeType, pageNumber) {
         loadThumbnail(themeId);
     });
     isLoading = false;
-    if (previewThemeId && themeIds.includes(previewThemeId)) {
+    if (previewThemeId && themesDb[previewThemeId] && themesDb[previewThemeId].themeType === themeType) {
         openPreview(previewThemeId);
         previewThemeId = null;
     }
@@ -80,8 +80,8 @@ function loadThumbnailGrid(themeType, pageNumber) {
 
 function openPreview(themeId) {
     $('#previewFrame').attr('src', 'preview/' + themeId + '.html');
-    $('#downloadButton').html('<i class="fa fa-download"></i> ' + $('#download_' + themeId).attr('title'));
-    $('#downloadButton').attr('href', $('#download_' + themeId).attr('href'));
+    $('#downloadButton').html('<i class="fa fa-download"></i> Download (' + themesDb[themeId].fileSize.toString() + ' MB)');
+    $('#downloadButton').attr('href', themesDb[themeId].themeUrl);
     $('#downloadButton').click(function() { clickCounter(themeId); });
     $('#previewModal').modal();
 }
